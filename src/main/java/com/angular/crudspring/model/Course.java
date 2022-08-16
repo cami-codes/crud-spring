844a0b8244ecfe150572a528bc41d06a60c3c37e
Camila <camila.matosrocha1234@gmail.com>
2022-08-16 12:58:00 -0300

    Consumo da API feita no Angular

--- src/main/java/com/angular/crudspring/model/Course.java
+++ src/main/java/com/angular/crudspring/model/Course.java
@@ -5,7 +5,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
+//import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.Data;
 
@@ -14,14 +16,15 @@ import lombok.Data;
 //@Table(name = "cursos")
 public class Course {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Long id;
-	
-	@Column(length = 200, nullable = false)
-	private String name;
-	
-	@Column(length = 20, nullable = false)
-	private String category;
-	
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @JsonProperty("_id")
+    private Long id;
+
+    @Column(length = 200, nullable = false)
+    private String name;
+
+    @Column(length = 10, nullable = false)
+    private String category;
+
 }
